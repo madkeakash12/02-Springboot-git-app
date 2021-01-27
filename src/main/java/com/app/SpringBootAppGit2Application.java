3acf6e72e88391b9c260076ec74409fb2db55a01
madkeakash12 <madkeakash12@users.noreply.github.com>
2021-01-27 07:16:13 +0530

    initial commit

---
+++ src/main/java/com/app/SpringBootAppGit2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppGit2Application.class, args);
+	}
+
+}
