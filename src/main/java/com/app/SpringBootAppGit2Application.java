633a5cdce9364fcfa02833d79a9e1b790b88c7df
madkeakash12 <madkeakash12@users.noreply.github.com>
2021-01-27 07:29:07 +0530

    f:202-his-02

--- src/main/java/com/app/SpringBootAppGit2Application.java
+++ src/main/java/com/app/SpringBootAppGit2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppGit2Application {
+int b=20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppGit2Application.class, args);
